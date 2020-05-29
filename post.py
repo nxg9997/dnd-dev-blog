00f545e66b595ea4e635a0bac8d42732a227f40d
Nathan Glick <nxg9997@rit.edu>
2020-05-29 18:22:43 -0400

    python script

---
+++ post.py
@@ -0,0 +1,21 @@
+import datetime
+import os
+
+title = input('Post Title: ')
+
+dt = datetime.datetime.now()
+
+date = dt.strftime('%Y-%m-%d')
+print(date)
+
+f = open('./_posts/' + date + '-' + title + ".md", "w")
+
+head = '---\nlayout: post\ntitle: "' + title + '"\ndate: ' + date + ' ' + dt.strftime('%X') + ' ' + '-0400' + '\ncategories: update\n---'
+
+f.write(head)
+
+print('Created new post')
+
+f.close()
+
+os.system('cd _posts && ' + date + '-' + title + ".md")
